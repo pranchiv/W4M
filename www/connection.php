4f89898bf87a41befa28a5b6fd60480040641184
Brian Stephens <brian@brimer.net>
2018-02-04 13:43:42 -0500

    put PDO class back

--- www/connection.php
+++ www/connection.php
@@ -1,7 +1,7 @@
 <?php
 require_once('appSettings.php');
 
-//echo '<div class="location_id">connection.php</div><br/>' . "\n\n";
+echo '<div class="location_id">connection.php</div><br/>' . "\n\n";
 
 class DB {
     private static $instance = NULL;
@@ -47,4 +47,31 @@ class DB {
         return $recordset;
     }
 }
+
+class DBPDO {
+    private static $instance = NULL;
+    
+    private function __construct() {}
+    private function __clone() {}
+    
+    public static function getInstance() {
+        //echo '<div class="location_id">DB getInstance()</div><br/>' . "\n\n";
+    
+        if (!isset(self::$instance)) {
+            try {
+                $connection_string = "mysql:host=" . DB_SERVER . ";dbname=" . DB_DBNAME; $u = DB_USERNAME; $p = DB_PASSWORD;
+                
+                echo "<p>Trying connection string: <span style=\"font-size: 8px;\">$connection_string</span></p>";
+    
+                self::$instance = new PDO($connection_string, $u, $p);
+    
+            } catch (PDOException $e) {
+                die("Error: " . $e->getMessage() . "<br/>\n");
+            }
+        }
+    
+        return self::$instance;
+    }
+}
+   
 ?>
