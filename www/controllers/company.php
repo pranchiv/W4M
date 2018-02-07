da00ed2f10e5f66df75598b54cecd9e379c0fef6
Brian Stephens <brian@brimer.net>
2018-02-07 11:04:18 -0500

    wired up sample2 AJAX call

--- www/controllers/company.php
+++ www/controllers/company.php
@@ -1,8 +1,8 @@
 <?php
-require_once('connection.php');
+require_once('../connection.php');
 
 $controller = new CompanyController();
-$controller->{ 'register' }();
+$controller->{ $_REQUEST['action'] }();
 
 class CompanyController {
     public function register() {
@@ -10,13 +10,19 @@ class CompanyController {
         header('content-type:application/json');
 
         // use form data $_POST array as parameters for DB call
-        $company = DB::callProcWithRecordset("CALL RegisterCompany(1, 'name', 'address1', 'address2', 'city', 'state', 'zip', 1)");
+        // put single quotes around any text fields: '$_POST[CompanyName]'
+        // no quotes are necessary around numeric fields: $_POST[CompanyType]
+        $DBResult = DB::callProcWithRecordset("CALL RegisterCompany(1, '$_POST[CompanyName]', 'address1', 'address2', 'city', 'state', 'zip', 1)");
 
-        if (is_null($company)) {
-            // error
+        if (is_null($DBResult)) {
+            $result = array('error' => true, 'errorMessage' => 'Database error');
         } else {
-            if ($company[0]['Exists'] == 1) {
+            $company = $DBResult[0];
 
+            if ($company['Exists'] == 1) {
+                $result = array('error' => false, 'exists' => true);
+            } else {
+                $result = array('error' => false, 'exists' => false, 'company' => $company);
             }
         }
 
--- www/scripts/sample.js
+++ www/scripts/sample.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-    $('#register_register').on('click', function(e) {
-        // AJAX call goes here
-        
-        alert('registered');
+    $('#register_registerButton').on('click', function(e) {
+        e.preventDefault();
+        var name = $('#register_companyName').val();
+
+        $.post('controllers/company.php?action=register', $('#register_form').serialize(), function(data) {
+
+            if (data.error) {
+                $('#register_registerError').html(data.errorMessage);
+            } else if (data.exists) {
+                $('#register_registerError').html('Error: "' + name + '" already exists.');
+            } else {
+                $('#register_registerError').html('Company created (ID ' + data.company.CompanyID + ')');
+            }
+
+        }, 'json');
     });
 });
