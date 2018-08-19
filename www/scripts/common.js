9adfef971a741308caae30c30c64901061e2da33
Brian Stephens <brian@brimer.net>
2018-08-19 15:08:23 -0400

    check for notification toast

--- www/scripts/common.js
+++ www/scripts/common.js
@@ -85,15 +85,17 @@ function BuildHtmlTableFromJson(data, columns) {
 // the "data" parameter is the JSON data returned from Notification.send()
 // it's expected to have a "recipients" array with specific properties
 function ShowToastFromNotificationSend(data) {
-    for (var i = 0; i < data.recipients.length; i++) {
-        var name = data.recipients[i]["Username"];
-        var address = data.recipients[i]["TextAddress"];
+    if (data.recipients) {
+        for (var i = 0; i < data.recipients.length; i++) {
+            var name = data.recipients[i]["Username"];
+            var address = data.recipients[i]["TextAddress"];
 
-        $.toast({ 
-            text: '<u>Text to ' + name + ' (' + address + ')</u><br>' + data.body,
-            allowToastClose: true,
-            hideAfter: false
-        });    
+            $.toast({ 
+                text: '<u>Text to ' + name + ' (' + address + ')</u><br>' + data.body,
+                allowToastClose: true,
+                hideAfter: false
+            });    
+        }
     }
 }
 
